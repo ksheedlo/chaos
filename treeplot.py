97d5cd5b77675636efbc70cc9e22d918c0c3b42f
Ken Sheedlo <ovrkenthousand@gmail.com>
2013-02-05 14:10:24 -0700

    Finish PS3.

--- treeplot.py
+++ treeplot.py
@@ -19,6 +19,19 @@ import sys
 from utils import split_dict
 from matplotlib.lines import Line2D
 
+def rotate(vec, theta):
+    '''
+    Rotates a 2D vector theta degrees.
+
+    '''
+    cos_theta = numpy.cos(theta)
+    sin_theta = numpy.sin(theta)
+    translation = numpy.array([
+                                [cos_theta, -sin_theta],
+                                [sin_theta, cos_theta]
+                            ], numpy.float64)
+    return numpy.dot(vec, translation)
+
 def render(x_start=1.0, y_start=0.5, size=1.0, iterations=13, **kwargs):
     '''
     Draws a neat fractal tree. 
@@ -62,6 +75,64 @@ def render(x_start=1.0, y_start=0.5, size=1.0, iterations=13, **kwargs):
     axes.set_ybound((0.0, 1.4))
     axes.set_title(opts.get('title', 'Self-similar Fractal Tree'))
 
+    if opts.get('filename') is not None:
+        figure.savefig(opts['filename'], dpi=220)
+    else:
+        figure.show()
+
+def skewed(x_start=1.0, y_start=0.5, size=1.0, iterations=13, **kwargs):
+    '''
+    Draws a fractal tree with tuneable parameters.
+
+    '''
+    opts, plot_args = split_dict((
+                                'title', 
+                                'filename', 
+                                'llength', 
+                                'rlength', 
+                                'ltheta', 
+                                'rtheta'
+                            ), kwargs)
+    llength = opts.get('llength', 0.6)
+    rlength = opts.get('rlength', 0.6)
+    render_color = opts.get('color', 'k')
+    ltheta = -numpy.deg2rad(opts.get('ltheta', 90.0))
+    rtheta = numpy.deg2rad(opts.get('rtheta', 90.0))
+
+    def _iterate(axes, p0, p1, iterations):
+        '''
+        Iterates a fancy fractal tree over the axes.
+
+        '''
+        if iterations == 0:
+            return
+
+        x0, y0 = p0
+        x1, y1 = p1
+        dv = numpy.array([x1 - x0, y1 - y0], numpy.float64)
+
+        lvec = llength * rotate(dv, ltheta)
+        rvec = rlength * rotate(dv, rtheta)
+
+        axes.plot((x1, x1+lvec[0]), (y1, y1+lvec[1]), color=render_color, 
+                    **plot_args)
+        _iterate(axes, p1, (x1+lvec[0], y1+lvec[1]), iterations-1)
+        axes.plot((x1, x1+rvec[0]), (y1, y1+rvec[1]), color=render_color, 
+                    **plot_args)
+        _iterate(axes, p1, (x1+rvec[0], y1+rvec[1]), iterations-1)
+
+    figure = matplotlib.pyplot.figure()
+    axes = figure.gca()
+    axes.plot(
+            (x_start, x_start), 
+            (y_start - (size/2), y_start + (size/2)), 
+            color=render_color, 
+            **plot_args
+        )
+    _iterate(axes, (x_start, y_start-(size/2)), (x_start, y_start+(size/2)), 
+                iterations)
+
+    axes.set_title(opts.get('title', 'Rotated Fractal Tree'))
     if opts.get('filename') is not None:
         figure.savefig(opts['filename'], dpi=220)
     else:
